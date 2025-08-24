022450a1dd833be5766a57358e8b04a8b9f3506f
nivlevyy <167879207+nivlevyy@users.noreply.github.com>
2025-08-24 08:09:23 +0300

    delete testing

--- data/pd_order_to_json.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import json
-
-if __name__ == '__main__':
-    new_data = pd.read_csv('flattened_urls.csv')
-    mylist = list(new_data['URL'])
-
-    with open('open_phish_data_new.json', 'w') as f:
-        json.dump(mylist, f, indent=2)
